e8174f84010bbcbb97339e8e66d3a54b8f48f99c
Senior-S <maximatador23@gmail.com>
2024-12-20 13:26:26 -0300

    Add project files.

---
+++ UnityExplorer/ProjectMain.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using SDG.Framework.Modules;
+using SDG.Unturned;
+
+namespace UnityExplorer;
+
+public class ProjectMain : IModuleNexus
+{
+    ExplorerStandalone explorerStandalone;
+
+    Harmony Harmony = new Harmony("com.unityexplorer.module");
+
+    public void initialize()
+    {
+        ExplorerStandalone.OnLog += ExplorerStandalone_OnLog;
+        explorerStandalone = ExplorerStandalone.CreateInstance();
+
+        Harmony.PatchAll(this.GetType().Assembly);
+
+        CommandWindow.LogWarning("UnityExplorer integration module loaded");
+    }
+
+    private void ExplorerStandalone_OnLog(string arg1, UnityEngine.LogType arg2)
+    {
+        CommandWindow.Log($"[ExplorerStandalone]<{arg2.ToString()}> {arg1}");
+    }
+
+    public void shutdown()
+    {
+        Harmony.UnpatchAll(this.Harmony.Id);
+
+        CommandWindow.Log("UnityExplorer unloaded");
+    }
+}
---
+++ UnityExplorer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnityExplorer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnityExplorer")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1eeed06a-5ad2-4b31-8821-08c1629a416d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
